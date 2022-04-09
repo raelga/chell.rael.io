1cb786427db458b60255210d2c78ed8909e7eca4
raelga <me@rael.io>
2022-04-09 07:42:19 +0000

    x

--- docker/traefik/start.sh
+++ docker/traefik/start.sh
@@ -1,6 +1,7 @@
 docker run -d --restart always \
   -p 80:80 -p 8080:8080 \
-  -v `pwd`/context/traefik.toml:/etc/traefik/traefik.toml \
+  -v `pwd`/config/traefik.toml:/etc/traefik/traefik.toml \
+  -v `pwd`/config/chell-rules.toml:/etc/traefik/chell-rules.toml \
   -v /var/run/docker.sock:/var/run/docker.sock \
   --name traefik \
   traefik
